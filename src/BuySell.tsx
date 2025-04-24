0f5892793cec738f41d8571f1a0b582262477f84
marscodeshere <102072700+marscodeshere@users.noreply.github.com>
2025-04-24 14:25:09 -0400

    Moved modal
    Moved modal to a different spot to check if it breaks.

--- src/BuySell.tsx
+++ src/BuySell.tsx
@@ -104,27 +104,27 @@ export default function BuySell() {
                                 </tbody>
                             </Table>
                         </Accordion.Body>
+                        <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Modal title</Modal.Title>
+                            </Modal.Header>
+                                <Modal.Body>
+                                I will not close if you click outside me. Do not even try to press
+                                escape key. {s.name}
+                                </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={handleClose}>
+                                    Cancel
+                                </Button>
+                                <Button variant="primary">Confirm</Button>
+                            </Modal.Footer>
+                        </Modal>
                     </Accordion.Item>
                     ))}   
                     </Accordion>
 
                 </Card.Body>
             </Card>
-            <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal title</Modal.Title>
-                </Modal.Header>
-                    <Modal.Body>
-                    I will not close if you click outside me. Do not even try to press
-                    escape key.
-                    </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary">Understood</Button>
-                </Modal.Footer>
-            </Modal>
         </Container>
 
         
